3d46a6cb2bd547fd410dbb76ff4f08823b4b4f3a
Vamba45 <vambafool@bk.ru>
2024-09-12 16:21:10 +0300

    add authrouter require

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const PORT = 5000;
 const cors = require('cors');
+const authRouter = require('./routes/authRouter');
 require('dotenv').config();
 
 const app = express();
