edd721bc117947023cbe6e3d3f8793e46431865e
Vamba45 <vambafool@bk.ru>
2024-09-12 16:18:59 +0300

    add auth router

--- controller/auth_controller.js
+++ controller/auth_controller.js
@@ -1,35 +1,6 @@
 const db = require('../pool');
 
 class authController {
-    async registration(req, res) {
-        try {
-            const {username, password} = req.body;
-
-            console.log(username, password);
-
-            const candidate = await db.query(`SELECT * FROM "Users" WHERE username = '${username}'`);
-
-            if(candidate.rowCount > 0) {
-                return res.status(400).json({message: "Пользователь с таки именем существует"});
-            }
-
-            const hashPassword = bcrypt.hashSync(password, 7);
-            
-            const user = await db.query(`INSERT INTO "Users" (username, "password") VALUES ('${username}', '${hashPassword}')`);
-        } catch(e) {
-            console.log(e);
-            res.status(400).json({message: 'Registration error'})
-        }
-    }
-
-    async login(req, res) {
-        try {
-
-        } catch {
-
-        }
-    }
-
     async getUser(req, res) {
         try {
             const user = await db.query(`SELECT * FROM users WHERE id = 1`);
--- index.js
+++ index.js
@@ -9,6 +9,6 @@ app.use(cors());
 app.use(express.json());
 
 // app.use('/api', userRouter);
-// app.use('/auth', authRouter);
+app.use('/auth', authRouter);
 
 app.listen(PORT, () => console.log(`server started at port: ${PORT}`));
