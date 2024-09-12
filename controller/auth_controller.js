bbdc14a14811c241d91fca5487ede4f943e9a9cd
Vamba45 <vambafool@bk.ru>
2024-09-12 20:59:35 +0300

    change res on auth controller

--- controller/auth_controller.js
+++ controller/auth_controller.js
@@ -4,7 +4,7 @@ class authController {
     async getUser(req, res) {
         try {
             const user = await db.query(`SELECT * FROM users`);
-            res.json(user.rows);
+            res.json(user.rows[0]);
         } catch (e) {
             res.status(400).json({message: 'Registration error'})
         }
