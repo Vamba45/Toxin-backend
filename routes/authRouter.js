abdd2108775599ae7cf64fa54f549c28c3a708c9
Vamba45 <vambafool@bk.ru>
2024-09-12 15:30:41 +0300

    delete useless vars from dotenv

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -2,8 +2,8 @@ const Router = require('express');
 const router = new Router();
 const controller = require('../controller/auth_controller.js');
 
-router.post('/registration', controller.registration);
-router.post('/login', controller.login);
+// router.post('/registration', controller.registration);
+// router.post('/login', controller.login);
 router.get('/user', controller.getUser);
 
 module.exports = router;
