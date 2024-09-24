c90530d77ac27713f39e1629c227e605b7f98b44
Vamba45 <vambafool@bk.ru>
2024-09-25 01:36:01 +0300

    remove unload path from generator

--- generator.js
+++ generator.js
@@ -24,7 +24,6 @@ function createRoom() {
         "https://premium.tophotels.ru/icache/reviewimage/477d54e05043c52d39f6a3b0c05e0c01/89.jpg",
         "https://avatars.mds.yandex.net/get-altay/1001354/2a000001661908ac62fa13fe7e5a12b08e71/XXL",
         "https://blog.postel-deluxe.ru/wp-content/uploads/hm/1e0daccaf1c9a5f55b3277e752c6d64553d41b083b0ebca68b4348bbad127daa.jpeg",
-        "https://uahotels.info/cache/images/4/e/8/4e8e6070c4f28e3db2605fef5fdd0d75.jpg",
         "https://www.rghk.com.hk/uploads/images/gallery-03-170728105100.jpg",
         "https://avatars.mds.yandex.net/i?id=a9cf09051534f4d3b7ad3f546dd6a5fe_l-4869221-images-thumbs&n=13",
         "https://i.pinimg.com/originals/85/29/0e/85290e2dfe74ab9e44351d2de2c8ee16.jpg",
