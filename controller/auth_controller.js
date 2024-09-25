5b4716f7286d3d52d990356e8565fa951f1a549e
Vamba45 <vambafool@bk.ru>
2024-09-25 20:38:14 +0300

    add CRUD to user API

--- controller/auth_controller.js
+++ controller/auth_controller.js
@@ -3,10 +3,43 @@ const db = require('../pool');
 class authController {
     async getUser(req, res) {
         try {
-            const user = await db.query(`SELECT * FROM users`);
+            const {email, password} = req.query;
+
+            const user = await db.query(`SELECT * FROM "user" WHERE email = '${email}' and "password" = '${password}'`);
+
             res.json(user.rows[0]);
         } catch (e) {
             res.status(400).json({message: 'Registration error'})
+        }
+    }
+
+    async createUser(req, res) {
+        try {
+            const {email, password, gender, birthdate, name, surname} = req.body;
+
+            const check = await db.query(`SELECT * FROM "user" WHERE email = '${email}' or ("name" = '${name}' and surname = '${surname}')`); 
+            
+            if(check.rowCount > 0) {
+                throw new Error();
+            }
+
+            await db.query(`INSERT INTO "user" ("password", email, "name", surname, birthdate, gender) VALUES ('${password}', '${email}', '${name}', '${surname}', '${birthdate}', ${gender})`);
+
+            res.json({message: 'Success'}); 
+        } catch(e) {
+            res.status(400).json({message: 'Insert data error'})
+        }
+    }
+
+    async removeUser(req, res) {
+        try {
+            const email = req.query.email;
+
+            await db.query(`DELETE FROM "user" WHERE email = '${email}'`);
+
+            res.json({message: 'Success'});
+        } catch(e) {
+            res.status(400).json({message: 'Remove data error'})
         }
     }
 }
